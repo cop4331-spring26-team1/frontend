01fc816b5744062d21125318db8f28176053ae33
4-KRussell <knrussell2006@gmail.com>
2026-02-02 20:07:14 -0500

    Resolved Syntax errors in AddContact API.

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -7,17 +7,17 @@
     $phone = $inData["phone"] ?? "";
     $email = $inData["email"] ?? "";
 
-    $userId = $inData["userId"] "" 0;
+    $userId = $inData["userId"] ?? 0;
     
     // Connect to mysql server as superuser
-    $conn = new mysqli("localhost", "localUser", "SmallProject", "SmallProject-DB");
+    $conn = new mysqli("localhost", "localUser", "SmallProject", "SmallProject_DB");
 
     if($conn -> connect_error){
         returnWithError($conn -> connect_error);
     }
     else{
         // Insert new Contact into Contacts table in SmallProject-DB
-        $stmt = $conn -> prepare("INSERT into Contacts (firstName, lastName, phone, email, userId) VALUES(?,?,?,?,?)");
+        $stmt = $conn -> prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserId) VALUES(?,?,?,?,?)");
         $stmt -> bind_param("ssssi", $firstName, $lastName, $phone, $email, $userId);
 
         if($stmt -> execute()){
