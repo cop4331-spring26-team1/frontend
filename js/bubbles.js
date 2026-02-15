851b331154fd748f7d1dbc4a91355c2a09405938
Liam Abernathy <liamab@ucf.edu>
2026-02-15 11:05:11 -0500

    [feature] bubble popping

--- js/bubbles.js
+++ js/bubbles.js
@@ -9,6 +9,54 @@
 
   const rand = (min, max) => Math.random() * (max - min) + min;
 
+  function popBubble(bubble) {
+    // Get bubble position and size
+    const rect = bubble.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const size = rect.width;
+    
+    // Add pop class to trigger animation
+    bubble.classList.add("bubble-popping");
+    
+    // Create particle fragments for explosion effect
+    const particleCount = 6;
+    for (let i = 0; i < particleCount; i++) {
+      const particle = document.createElement("div");
+      particle.className = "bubble-particle";
+      
+      const angle = (Math.PI * 2 * i) / particleCount;
+      const distance = size * 0.8;
+      const endX = Math.cos(angle) * distance;
+      const endY = Math.sin(angle) * distance;
+      const particleSize = size * 0.3;
+      
+      // Position particle at center, then it will animate outward
+      particle.style.left = (centerX - particleSize / 2) + "px";
+      particle.style.top = (centerY - particleSize / 2) + "px";
+      particle.style.setProperty("--endX", endX + "px");
+      particle.style.setProperty("--endY", endY + "px");
+      particle.style.width = particleSize + "px";
+      particle.style.height = particleSize + "px";
+      
+      layer.appendChild(particle);
+      
+      // Remove particle after animation
+      setTimeout(() => {
+        if (particle.parentNode) {
+          particle.remove();
+        }
+      }, 300);
+    }
+    
+    // Remove bubble after animation completes
+    setTimeout(() => {
+      if (bubble.parentNode) {
+        bubble.remove();
+      }
+    }, 300);
+  }
+
   function spawnBubble() {
     const b = document.createElement("div");
     b.className = "bubble";
@@ -31,10 +79,18 @@
     b.style.setProperty("--xdriftStart", driftStart);
     b.style.setProperty("--xdriftEnd", driftEnd);
 
+    // Add hover event listener for easter egg
+    b.addEventListener("mouseenter", () => popBubble(b));
+    b.style.cursor = "pointer";
+
     layer.appendChild(b);
 
     const ttlMs = (dur + delay) * 1000 + 250;
-    window.setTimeout(() => b.remove(), ttlMs);
+    window.setTimeout(() => {
+      if (b.parentNode) {
+        b.remove();
+      }
+    }, ttlMs);
   }
 
   window.setInterval(() => {
